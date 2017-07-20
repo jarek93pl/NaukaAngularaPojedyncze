e8a0b0856120bf010ad46c60183e59e51bdf336d
jarek <jarek@DESKTOP-AT84MKT>
2017-07-20 11:39:40 +0200

    rozdział 8

---
+++ Angular/Scripts/Filter/Filtr.js
@@ -0,0 +1,5 @@
+﻿angular.module("exampleApp.Filters", []).filter("dayName", function ()
+{
+    var dayNames = ["niedziela", "poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota"];
+    return function (input) { return angular.isNumber(input) ? dayNames[input] : input; };
+});
