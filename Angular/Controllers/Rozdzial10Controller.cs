caf9bd67a4aa1bda8ebc700794a905f32ef916a5
jarek <jarek@DESKTOP-AT84MKT>
2017-07-21 08:35:34 +0200

    a

---
+++ Angular/Controllers/Rozdzial10Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Angular.Controllers
+{
+    public class Rozdzial10Controller : Controller
+    {
+        // GET: Rozdzial10
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
