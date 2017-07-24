09411279362bb93fb33cd8ee0c412a2710f550ff
jarek <jarek@DESKTOP-AT84MKT>
2017-07-24 14:19:45 +0200

    420

---
+++ Angular/Controllers/R16DyrektywyController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Angular.Controllers
+{
+    public class R16DyrektywyController : Controller
+    {
+        // GET: R16Dyrektywy
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DwaZakresyISzablon()
+        {
+            return View();
+        }
+    }
+}
