e4a244846f94cb3b34173acc6b767ef96aec2394
jarek <jarek@DESKTOP-AT84MKT>
2017-07-25 15:17:10 +0200

    540str

---
+++ Angular/Controllers/R20ObietnicaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Angular.Controllers
+{
+    public class R20ObietnicaController : Controller
+    {
+        // GET: R20Obietnica
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Angular/Controllers/R21RestFullController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Angular.Controllers
+{
+    public class R21RestFullController : Controller
+    {
+        // GET: R21RestFull
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
